ac8798f2de8190bef9720146bdf16763731288b7
Csabán Balázs <Ba.Csaban@eos-ts.com>
2024-01-05 16:24:57 +0100

    [routing] added counter to router

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -6,9 +6,8 @@ function Menu() {
         <div class="menu-wrapper">
             <MetaProvider >
                 <Title>SolidStart - Basic</Title>
-                <a href="/">Index</a>
+                <a href="/counter">Counter</a>
                 <a href="/list/list">List</a>
-                <a href="/about">About</a>
             </MetaProvider>
         </div>
     )
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,12 @@
 import { Route, Router } from '@solidjs/router'
 import List from '../components/list/List'
 import Item from '../components/item/Item'
+import Counter from '../components/counter/Counter'
 
 function RouterComponent() {
   return (
       <Router>
+        <Route path="/counter" component={Counter} />
         <Route path="/list/list" component={List} />
         <Route path="/list/:characterId" component={Item} />
       </Router>
